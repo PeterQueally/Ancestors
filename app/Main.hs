960158ef5fc7289e99b643baae704d50b05e3737
PeterQueally <queallp@tcd.ie>
2018-09-30 18:23:54 +0100

    Creating binary tree

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,6 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do let tip n = Node n Nil Nil
+          let tree = Node 8 (Node 3 (tip 1) (Node 6 (tip 4) (tip 7)))
+                            (Node 10 Nil (Node 14 (tip 13) Nil))
